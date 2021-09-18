8a1dcb495dc1532a45c27f9ee24c4c9b293f2e07
Marcos Maverick <marquinhopimpo@gmail.com>
2021-09-17 21:15:02 -0300

    Responsividade e visual de links ativos

--- src/components/Sidebar/SideBarNav.tsx
+++ src/components/Sidebar/SideBarNav.tsx
@@ -7,15 +7,15 @@ export function SideBarNav () {
     return (
         <Stack spacing="12" align="flex-start">
                 <NavSection title="GERAL">
-                    <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-                    <NavLink icon={RiContactsLine}>Usuários</NavLink>
+                    <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
+                    <NavLink icon={RiContactsLine } href="/users">Usuários</NavLink>
                 </NavSection>
                 <Box>
                     <Text fontWeight="bold" color="gray.400" fontSize="small">Automação</Text>
                     <Stack spacing="4" mt="8" align="stretch">
                         <NavSection title="AUTOMAÇÃO">
-                            <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-                            <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+                            <NavLink icon={RiInputMethodLine} href="/forms">Formulários</NavLink>
+                            <NavLink icon={RiGitMergeLine} href="/automation">Automação</NavLink>
                         </NavSection>
                     </Stack>
                 </Box>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box,Flex,Heading,Button,Icon,Table,Thead,Tr,Th,Checkbox,Tbody,Td,Text } from '@chakra-ui/react'
+import { Box,Flex,Heading,Button,Icon,Table,Thead,Tr,Th,Checkbox,Tbody,Td,Text,useBreakpointValue } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
-
+import Link from 'next/link'
 export default function UserList() {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true 
+    })
     return (
         <Box>
             <Header />
@@ -14,23 +19,25 @@ export default function UserList() {
                 <Box flex="1" borderRadius="8" bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} cursor="pointer">Criar novo</Button>
+                        <Link href="/users/create" passHref>
+                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} cursor="pointer">Criar novo</Button>
+                        </Link>
+                        
                     </Flex>
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4" , "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"/>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                { isWideVersion &&  <Th>Data de cadastro</Th> }
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">  
+                                <Td px={["4" , "4", "6"]}>  
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -39,13 +46,14 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">marquinhopimpo@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>12 de Setembro de 2021</Td>
+                                { isWideVersion && <Td>12 de Setembro de 2021</Td> }
                                 <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>} cursor="pointer">Editar</Button>
+                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>} cursor="pointer">{ isWideVersion ? 'Editar' : '' }</Button>
                                 </Td>
                             </Tr>
                         </Tbody>
                     </Table>
+                    
                     <Pagination />
                 </Box>
             </Flex>
